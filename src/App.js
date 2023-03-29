d55ae367a01c7d390fb9f637680040ca5d78bbcf
surazregmy <suraz.regmy@gmail.com>
2023-03-28 21:01:47 -0400

    added project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import KismatBoka from "../src/img/kismat.jpeg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={KismatBoka} className="App-logo" alt="logo" />
+        <br></br>
+        <p>Ma Ekdam thulo boka ho! Maile Kt haru lai esari nai ghumauxu</p>
+        <p>Malai Esma gali garnu hola</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/kismath.pantha.9"
           target="_blank"
           rel="noopener noreferrer"
         >
